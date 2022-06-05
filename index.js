4dcb61eba7427da82195c5d3db03d4cbcbb95974
Diego Ramirez <gustavormzrmz@gmail.com>
2022-06-05 15:38:30 -0500

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 import controller  from './lib/controller/song' 
 
 const app = express()
 const port = 8080
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
